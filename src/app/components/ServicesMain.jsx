272106ed97b733b0f9f150364fabd98952b0a3a7
sjhmichael <65953182+sjhmichael@users.noreply.github.com>
2024-05-07 02:13:19 +0800

    update

--- src/app/components/ServicesMain.jsx
+++ src/app/components/ServicesMain.jsx
@@ -8,17 +8,20 @@ import LightningIcon from "../../../public/icons/LightningIcon";
 
 function ServicesMain() {
 
-  if (typeof window !== 'undefined') {
-    document.getElementById("cards").onmousemove = (e) => {
-      for (const card of document.getElementsByClassName("card")) {
-        const rect = card.getBoundingClientRect(),
-          x = e.clientX - rect.left,
-          y = e.clientY - rect.top;
+  window.onload = function () {
+    if (typeof window !== 'undefined') {
+      document.getElementById("cards").onmousemove = (e) => {
+        for (const card of document.getElementsByClassName("card")) {
+          const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
 
-        card.style.setProperty("--mouse-x", `${x}px`);
-        card.style.setProperty("--mouse-y", `${y}px`);
-      }
-    };
+          card.style.setProperty("--mouse-x", `${x}px`);
+          card.style.setProperty("--mouse-y", `${y}px`);
+        }
+
+      };
+    }
   }
 
   return (
